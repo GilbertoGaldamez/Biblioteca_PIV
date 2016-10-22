48a96e4ff7a3ca07cc8d550acaed19983140233c
GilbertoGaldamez <gilberto.galdamez@unitec.edu>
2016-10-22 14:54:15 -0600

    Agregando Contexto y primer Modelo.

---
+++ Biblioteca/Biblioteca.Data/Modelos/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Data.Modelos
+{
+    public class Libro
+    {
+        // Propiedades:
+        public int Id { get; set; }     // Id: Framework lo coloca como llave primaria
+        public string Nombre { get; set; }
+    }
+}
